7dbfc41d3b04c37902580727fadffe51bbe4ddf0
Daniel Cosme <cosmedaniel8@gmail.com>
2021-04-27 18:29:46 -0400

    Implement isogram first round

---
+++ isogram/isogram.go
@@ -0,0 +1,26 @@
+// Package isogram provides functionality related to isogram words
+package isogram
+
+import (
+	"strings"
+)
+
+// IsIsogram determines if a word is an isogram or not
+func IsIsogram(word string) bool {
+	res := true
+	counter := make(map[rune]int)
+	word = strings.ToLower(word)
+
+	for _, char := range word {
+		if char == ' ' || char == '-' {
+			continue
+		}
+
+		counter[char]++
+		if counter[char] > 1 {
+			res = false
+			break
+		}
+	}
+	return res
+}
